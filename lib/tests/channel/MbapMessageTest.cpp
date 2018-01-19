939fdde2b5db83b0d0ebb67a8e68c1057b0a4dff
Émile Grégoire <eg@emilegregoire.ca>
2018-01-19 10:33:39 -0500

    Fixed warning about brace initializer for std::array. Thanks clang!

--- lib/tests/channel/MbapMessageTest.cpp
+++ lib/tests/channel/MbapMessageTest.cpp
@@ -12,13 +12,13 @@ TEST_CASE("MbapMessage")
     UnitIdentifier unit_id{0x42};
     TransactionIdentifier transaction_id{0x1234};
     RequestMock request{2, 0x76};
-    std::array<uint8_t, 9> raw_request_data {
+    std::array<uint8_t, 9> raw_request_data{{
             0x12, 0x34, // Transaction Identifier
             0x00, 0x00, // Protocol Identifier
             0x00, 0x03, // Length
             0x42,       // Unit identifier
             0x76, 0x76  // Data
-    };
+    }};
     openpal::rseq_t raw_request{raw_request_data.data(), raw_request_data.size()};
 
     SECTION("When constructor, then set the values appropriately")
@@ -47,12 +47,12 @@ TEST_CASE("MbapMessage")
         auto write_buffer = buffer.as_wslice();
         auto result = MbapMessage::build_message(unit_id, transaction_id, empty_request, write_buffer);
 
-        std::array<uint8_t, 7> expected_message_data{
+        std::array<uint8_t, 7> expected_message_data{{
                 0x12, 0x34, // Transaction Identifier
                 0x00, 0x00, // Protocol Identifier
                 0x00, 0x01, // Length
                 0x42,       // Unit identifier
-        };
+        }};
         openpal::rseq_t expected_message{expected_message_data.data(), expected_message_data.size()};
         REQUIRE(result.equals(expected_message));
     }
--- lib/tests/channel/MbapParserTest.cpp
+++ lib/tests/channel/MbapParserTest.cpp
@@ -21,13 +21,13 @@ TEST_CASE("MbapParser")
     MbapSinkMock sink;
     MbapParser parser{&sink};
 
-    std::array<uint8_t, 9> complete_message{
+    std::array<uint8_t, 9> complete_message{{
             0x12, 0x34, // Transaction Identifier
             0x00, 0x00, // Protocol Identifier
             0x00, 0x03, // Length
             0x42,       // Unit identifier
             0x98, 0x76  // Data
-    };
+    }};
 
     SECTION("When receive complete message, then report received message")
     {
@@ -86,11 +86,11 @@ TEST_CASE("MbapParser")
 
     SECTION("When reset, then stop parsing and reset internal state")
     {
-        std::array<uint8_t, 6> incomplete_message{
+        std::array<uint8_t, 6> incomplete_message{{
                 0x45, 0x67, // Transaction Identifier
                 0x00, 0x00, // Protocol Identifier
                 0x00, 0x03, // Length
-        };
+        }};
         openpal::rseq_t buffer{incomplete_message.data(), incomplete_message.size()};
         parser.parse(buffer);
 
@@ -104,13 +104,13 @@ TEST_CASE("MbapParser")
 
     SECTION("When receive wrong protocol identifier, then reject message")
     {
-        std::array<uint8_t, 9> wrong_protocol_id_message{
+        std::array<uint8_t, 9> wrong_protocol_id_message{{
                 0x12, 0x34, // Transaction Identifier
                 0x42, 0x42, // Protocol Identifier
                 0x00, 0x03, // Length
                 0x42,       // Unit identifier
                 0x98, 0x76  // Data
-        };
+        }};
         openpal::rseq_t buffer{wrong_protocol_id_message.data(), wrong_protocol_id_message.size()};
 
         parser.parse(buffer);
@@ -130,12 +130,12 @@ TEST_CASE("MbapParser")
 
     SECTION("When length is 0, then reject message")
     {
-        std::array<uint8_t, 7> length_zero_message{
+        std::array<uint8_t, 7> length_zero_message{{
                 0x12, 0x34, // Transaction Identifier
                 0x42, 0x42, // Protocol Identifier
                 0x00, 0x00, // Length
                 0x42,       // Unit identifier
-        };
+        }};
         openpal::rseq_t buffer{length_zero_message.data(), length_zero_message.size()};
 
         parser.parse(buffer);
@@ -155,13 +155,13 @@ TEST_CASE("MbapParser")
 
     SECTION("When length is greater than the maximum, then go to end of stream, but only report maximum size")
     {
-        std::array<uint8_t, 65541> length_max_message{
+        std::array<uint8_t, 65541> length_max_message{{
                 0x12, 0x34, // Transaction Identifier
                 0x00, 0x00, // Protocol Identifier
                 0xFF, 0xFF, // Length
                 0x42,       // Unit identifier
                 0x76        // Data (length = 65535 bytes)
-        };
+        }};
         openpal::rseq_t buffer{length_max_message.data(), length_max_message.size()};
 
         parser.parse(buffer);
