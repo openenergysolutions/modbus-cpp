5af1d45422652af580234f739aec346d42ee7a87
Émile Grégoire <eg@emilegregoire.ca>
2018-05-08 13:54:57 -0400

    Started documentation using Doxygen.

--- lib/include/modbus/Expected.h
+++ lib/include/modbus/Expected.h
@@ -1,7 +1,3 @@
-// This class is heavily inspired by Andrei Alexandescu `Expected` class
-// that he presented in 2012 in his talk "Systematic Error Handling in C++"
-// available on Channel9: https://channel9.msdn.com/Shows/Going+Deep/C-and-Beyond-2012-Andrei-Alexandrescu-Systematic-Error-Handling-in-C
-
 #ifndef MODBUS_EXPECTED_H
 #define MODBUS_EXPECTED_H
 
@@ -13,13 +9,56 @@
 namespace modbus
 {
 
+/**
+ * @brief Expected type often used in return values and callbacks
+ * @tparam T Type of the expected value
+ *
+ * This type represents a value that is expected to be returned, but may be impossible
+ * to obtain. If @ref is_valid() returns @cpp true @ce, it means that the value was indeed
+ * returned and it may be obtained by @ref get(). Otherwise, it means an error occured and the
+ * value could not be returned. In this case, you can determine the type with @ref has_exception()
+ * templated with the type of error it may have returned. You can also get the error with @ref get_exception().
+ *
+ * It is very similar to Rust's <a href="https://doc.rust-lang.org/std/result/">Result</a> type.
+ * This class uses the @cpp std::exception_ptr @ce union trick to work.
+ *
+ * This class is heavily inspired by Andrei Alexandescu `Expected` class that he presented in 2012
+ * in his talk "Systematic Error Handling in C++" available on Channel9:
+ * https://channel9.msdn.com/Shows/Going+Deep/C-and-Beyond-2012-Andrei-Alexandrescu-Systematic-Error-Handling-in-C
+ */
 template<typename T> class Expected
 {
 public:
+    /**
+     * @brief Constructor for a valid value
+     * @param value Value to be created
+     *
+     * This constructor is used whenever you want to create a valid value (i.e. no error) by copy.
+     * Here's an example of its usage:
+     * @cpp
+     * Expected<int> foo()
+     * {
+     *      return Expected<int>{42};
+     * }
+     * @ce
+     */
     Expected(const T& value) : m_value{value}, m_has_value{true} {}
 
+    /**
+     * @brief Constructor for a valid value
+     * @param rhs Value to be created
+     *
+     * This constructor is used whenever you want to create a valid value (i.e. no error) by move.
+     */
     Expected(T&& rhs) : m_value{std::move(rhs)}, m_has_value{true} {}
 
+    /**
+     * @brief Copy contructor
+     * @param rhs   Right-hand side value
+     *
+     * This copy constructor will create a valid value or an exception value based on the
+     * right-hand side value.
+     */
     Expected(const Expected& rhs)
         : m_has_value{rhs.m_has_value}
     {
@@ -33,6 +72,13 @@ public:
         }
     }
 
+    /**
+     * @brief Move contructor
+     * @param rhs   Right-hand side value
+     *
+     * This copy constructor will create a valid value or an exception value based on the
+     * right-hand side value.
+     */
     Expected(Expected&& rhs)
         : m_has_value{rhs.m_has_value}
     {
@@ -46,12 +92,32 @@ public:
         }
     }
 
+    /**
+     * @brief Destructor
+     *
+     * Effectively calls the destructor of the value or of the error.
+     */
     ~Expected()
     {
         if (m_has_value) m_value.~T();
         else m_exception.~exception_ptr();
     };
 
+    /**
+     * @brief Creates an error
+     * @tparam E Error type (can be deduced by the parameter)
+     * @param exception Error value
+     * @return New error for an expectation
+     *
+     * This method should be used when the value could not be obtained (i.e. an error occured).
+     * Here's an example usage:
+     * @cpp
+     * Expected<int> foo()
+     * {
+     *      return Expected<int>::from_exception("Could not foo...");
+     * }
+     * @ce
+     */
     template<typename E>
     static Expected<T> from_exception(const E& exception)
     {
@@ -62,6 +128,11 @@ public:
         return from_exception(std::make_exception_ptr(exception));
     }
 
+    /**
+     * @brief Creates an error from and @cpp std::exception_ptr @ce
+     * @param exception Error value as an @cpp std::exception_ptr @ce
+     * @return New error for an expectation
+     */
     static Expected<T> from_exception(std::exception_ptr exception)
     {
         Expected<T> result;
@@ -70,16 +141,28 @@ public:
         return result;
     }
 
-    static Expected<T> from_exception()
-    {
-        return from_exception(std::current_exception());
-    }
-
+    /**
+     * @brief Check if expectation is valid
+     * @return @cpp true @ce if the expectation is valid, @cpp false @ce if it contains an error
+     *
+     * If this method returns @cpp true @ce, the value can be obtained with @ref get(). Otherwise,
+     * the error can be obtained with @ref get_exception().
+     * @see @ref get() @ref has_exception() @ref get_exception()
+     */
     bool is_valid() const
     {
         return m_has_value;
     }
 
+    /**
+     * @brief Return the contained value
+     * @return Contained value
+     * @throw Re-throws the exception if it contains one
+     *
+     * @note Always check that @ref is_valid() return @cpp true @ce before calling this method.
+     * Otherwise, it may throw an exception.
+     * @see @ref is_valid()
+     */
     T& get()
     {
         if(!m_has_value)
@@ -89,6 +172,15 @@ public:
         return m_value;
     }
 
+    /**
+     * @brief Return the contained value
+     * @return Contained value
+     * @throw Re-throws the exception if it contains one
+     *
+     * @note Always check that @ref is_valid() return @cpp true @ce before calling this method.
+     * Otherwise, it may throw an exception.
+     * @see @ref is_valid()
+     */
     const T& get() const
     {
         if(!m_has_value)
@@ -98,6 +190,13 @@ public:
         return m_value;
     }
 
+    /**
+     * @brief Check if the expectation contains an error of the specified type
+     * @tparam E    Type of the error to check
+     * @return @cpp true @ce if it contains an error of the specified type, @cpp false @ce otherwise.
+     *
+     * @see @ref is_valid() @ref get_exception()
+     */
     template<typename E>
     bool has_exception() const
     {
@@ -113,27 +212,27 @@ public:
         return false;
     }
 
+    /**
+     * @brief Get the internal @cpp std::exception_ptr @ce
+     * @return Internal @cpp std::exception_ptr @ce
+     * @throw std::invalid_argument if the expectation is valid
+     *
+     * @see @ref is_valid()
+     */
     std::exception_ptr get_exception() const
     {
         if (m_has_value) throw std::invalid_argument("no exception");
         return m_exception;
     }
 
-    /*template<typename E>
-    E get_exception() const
-    {
-        try
-        {
-            if(!m_has_value) std::rethrow_exception(m_exception);
-        }
-        catch(const E& exception)
-        {
-            return exception;
-        }
-        catch(...) {}
-        throw std::invalid_argument("not the right exception");
-    }*/
-
+    /**
+     * @brief Get the error of the specified type
+     * @tparam E Error type
+     * @return Internal @cpp std::exception_ptr @ce
+     * @throw std::invalid_argument if the expectation does not contain the right type
+     *
+     * @see @ref is_valid() @ref has_exception()
+     */
     template<typename E>
     E get_exception() const
     {
