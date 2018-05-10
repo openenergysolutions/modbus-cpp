e874df23f3c7f767ab22c47574b64783af2fe92c
Émile Grégoire <eg@emilegregoire.ca>
2018-05-10 09:57:10 -0400

    Cleanup example.

--- example/main.cpp
+++ example/main.cpp
@@ -36,107 +36,101 @@ int main(int argc, char* argv[])
     // Create a console logger
     auto logger = modbus::LoggerFactory::create_console_logger("Hello");
 
+    // Create the modbus manager
+    // This will create the necessary background threads
+    std::unique_ptr<IModbusManager> modbusManager = IModbusManager::create(logger);
+
+    // Create a TCP channel
+    // Each channel has its own strand of execution
+    auto channel = modbusManager->create_tcp_channel("Example channel",
+                                                     Ipv4Endpoint{ "127.0.0.1", 502 });
+
+    // Create a session with a specific unit identifier
+    // Users will mainly play with the session to obtain what they want
+    auto session = channel->create_session(UnitIdentifier{0x01},
+                                           std::chrono::seconds(3),
+                                           std::make_shared<MySessionResponseHandler>());
+
+    // Schedule a recurring request
+    // All the scheduled requests will be handled by the ISessionResponseHandler registered on session creation
+    ReadHoldingRegistersRequest req{ 0x0024, 59 };
+    auto scheduled_req = session->schedule_request(req, std::chrono::seconds(2));
+
+    while (true)
     {
-        std::cout << "Start" << std::endl;
+        char cmd;
+        std::cin >> cmd;
+
+        switch (cmd)
         {
-            // Create the modbus manager
-            // This will create the necessary background threads, initialize Asio io_services
-            std::unique_ptr<IModbusManager> modbusManager = IModbusManager::create(logger);
-
-            // Create a TCP channel
-            // Each channel has its own Executor with a strand to avoid many multithreading issues
-            auto channel = modbusManager->create_tcp_channel("Example channel", Ipv4Endpoint{ "127.0.0.1", 502 });
-
-            // Create a session with a specific unit identifier
-            // Users will mainly play with the session to obtain what they want
-            auto session = channel->create_session(UnitIdentifier{0x01},
-                std::chrono::seconds(3),
-                std::make_shared<MySessionResponseHandler>());
-
-            // Schedule a recurring request
-            // All the scheduled requests will be handled by the ISessionResponseHandler registered on session creation
+        case 'a':
+        {
+            // Send a request and print the result
+            // You can override the default timeout value set when creating the session
             ReadHoldingRegistersRequest req{ 0x0024, 59 };
-            auto scheduled_req = session->schedule_request(req, std::chrono::seconds(2));
+            session->send_request(req, std::chrono::seconds(3), [](const Expected<ReadHoldingRegistersResponse>& response) {
+                // If the exception is set, then an error occured
+                if (!response.is_valid())
+                {
+                    std::cout << response.get_exception<IException>().get_message() << std::endl;
+                    return;
+                }
 
-            while (true)
+                // Otherwise, everything went good and the response is available
+                for (auto& value : response.get().values)
+                {
+                    std::cout << value.address << ": " << value.value << std::endl;
+                }
+            });
+            break;
+        }
+        case 'w':
+        {
+            WriteMultipleRegistersRequest req{ 0x0024, {11, 22, 33} };
+
+            session->send_request(req, [](const Expected<WriteMultipleRegistersResponse>& response) {
+                if (!response.is_valid())
+                {
+                    std::cout << response.get_exception<IException>().get_message() << std::endl;
+                    return;
+                }
+
+                std::cout << "Write result= " << response.get().starting_address << ": " << response.get().qty_of_registers << std::endl;
+            });
+            break;
+        }
+        case 'c':
+        {
+            if(scheduled_req->is_running())
             {
-                char cmd;
-                std::cin >> cmd;
-
-                switch (cmd)
-                {
-                case 'a':
-                {
-                    // Send a request and print the result
-                    // You can override the default timeout value set when creating the session
-                    ReadHoldingRegistersRequest req{ 0x0024, 59 };
-                    session->send_request(req, std::chrono::seconds(3), [](const Expected<ReadHoldingRegistersResponse>& response) {
-                        // If the exception is set, then an error occured
-                        if (!response.is_valid())
-                        {
-                            std::cout << response.get_exception<IException>().get_message() << std::endl;
-                            return;
-                        }
-
-                        // Otherwise, everything went good and the response is available
-                        for (auto& value : response.get().values)
-                        {
-                            std::cout << value.address << ": " << value.value << std::endl;
-                        }
-                    });
-                    break;
-                }
-                case 'w':
-                {
-                    WriteMultipleRegistersRequest req{ 0x0024, {11, 22, 33} };
-
-                    session->send_request(req, [](const Expected<WriteMultipleRegistersResponse>& response) {
-                        if (!response.is_valid())
-                        {
-                            std::cout << response.get_exception<IException>().get_message() << std::endl;
-                            return;
-                        }
-
-                        std::cout << "Write result= " << response.get().starting_address << ": " << response.get().qty_of_registers << std::endl;
-                    });
-                    break;
-                }
-                case 'c':
-                {
-                    if(scheduled_req->is_running())
-                    {
-                        scheduled_req->stop();
-                    }
-                    else
-                    {
-                        scheduled_req->start();
-                    }
-                    break;
-                }
-                case 'z':
-                {
-                    if(scheduled_req->get_frequency() == std::chrono::seconds(2))
-                    {
-                        scheduled_req->set_frequency(std::chrono::seconds(5));
-                    }
-                    else
-                    {
-                        scheduled_req->set_frequency(std::chrono::seconds(2));
-                    }
-                    break;
-                }
-                case 'q':
-                    // Quit
-                    goto end;
-
-                default:
-                    break;
-                }
-
+                scheduled_req->stop();
             }
+            else
+            {
+                scheduled_req->start();
+            }
+            break;
+        }
+        case 'z':
+        {
+            if(scheduled_req->get_frequency() == std::chrono::seconds(2))
+            {
+                scheduled_req->set_frequency(std::chrono::seconds(5));
+            }
+            else
+            {
+                scheduled_req->set_frequency(std::chrono::seconds(2));
+            }
+            break;
+        }
+        case 'q':
+            // Quit
+            return 0;
+
+        default:
+            break;
         }
     }
 
-end:
     return 0;
 }
